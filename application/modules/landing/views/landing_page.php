9ce903e30355e1ecf038ba91b0e6735f8a4618da
seffirzhn <152045353+seffirzhn@users.noreply.github.com>
2025-04-28 11:12:58 +0700

    committ

--- application/modules/landing/views/landing_page.php
+++ application/modules/landing/views/landing_page.php
@@ -9,6 +9,7 @@
     <link rel="Icon" type="png" href="resources/config_site/logo-tpi.png">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -53,8 +54,7 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 order-lg-0 mt-5 mt-lg-0 position-relative">
-                    <img src="assets/img/dotted-circle.svg" alt="Dotted" 
-                    style="position:absolute; top: -50px; left: -80px; width: 300px; opacity: 0.3; z-index: 0;">
+                    <img src="assets/img/dotted-circle.svg" alt="Dotted" class="dotted-circle">
                     <h1 class="display-5 fw-bold mb-3 position-relative" style="z-index:1;">Selamat Datang</h1>
                     <p class="lead text-muted position-relative" style="z-index:1;">Sistem Informasi Pendataan Aplikasi</p>
                 </div>
